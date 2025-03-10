28b0cd2f6caca7b5918dcdaeca7e88b0e363784c
Akshar-Parekh-Inventyv <akshar.parekh.inventyv@gmail.com>
2025-03-10 18:06:50 +0530

    ChatApp Prohect setup and firebase connection

---
+++ angular-chat-app/src/app/app.config.ts
@@ -0,0 +1,27 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)), 
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
+  ],
+};
